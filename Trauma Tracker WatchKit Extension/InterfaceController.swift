7e3981e7f4b060d3d1c970999ddf8efcc178dd7a
Adam Dama <adamdama1@gmail.com>
2019-02-08 01:47:34 -0600

    Initial Commit

---
+++ Trauma Tracker WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Trauma Tracker WatchKit Extension
+//
+//  Created by Adam Dama on 2/8/19.
+//  Copyright © 2019 Adam Dama. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
