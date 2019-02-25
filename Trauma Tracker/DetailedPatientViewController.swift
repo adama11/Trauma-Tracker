c608d55e022db6450fbc70c8ae6bac54303e8592
Adam Dama <adamdama1@gmail.com>
2019-02-24 21:30:13 -0600

    detail view ui

--- Trauma Tracker/DetailedPatientViewController.swift
+++ Trauma Tracker/DetailedPatientViewController.swift
@@ -10,12 +10,44 @@ import UIKit
 
 class DetailedPatientViewController: UIViewController {
     
+    @IBOutlet weak var pulseRateIcon: UIView!
+    @IBOutlet weak var respRateIcon: UIView!
+    @IBOutlet weak var bodyTempIcon: UIView!
+    @IBOutlet weak var spo2Icon: UIView!
+    @IBOutlet weak var bloodPressureIcon: UIView!
+    @IBOutlet weak var ESILabel: UILabel!
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+       
+        
     }
+    
+    override func viewDidLayoutSubviews() {
+        let vitalsItems: [UIView] = [pulseRateIcon, respRateIcon, bodyTempIcon, spo2Icon, bloodPressureIcon]
+        for v in vitalsItems {
+            drawIcon(square: v, color: UIColor.green.cgColor)
+        }
+        
+    }
+
+    
+    func drawIcon(square: UIView, color: CGColor) {
+        let inset: CGFloat = 4.0
+        let circlePath = UIBezierPath(ovalIn: square.bounds.insetBy(dx: inset, dy: inset))
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = circlePath.cgPath
+        shapeLayer.fillColor = color
+        //    shapeLayer.strokeColor = UIColor.redColor().CGColor
+        //    shapeLayer.lineWidth = desiredLineWidth
+        square.layer.addSublayer(shapeLayer)
+    }
+    
+    
+    
     
 
     /*
