0ec3d79eca68e39222a2ebb0f08d32a3d5c78cd1
tkeaty <tkeaty2@illinois.edu>
2019-02-24 18:06:09 -0600

    Empty backend

---
+++ Trauma Tracker/BackEndFunctions.swift
@@ -0,0 +1,10 @@
+//
+//  BackEndFunctions.swift
+//  Trauma Tracker
+//
+//  Created by Tommy Keaty on 2/24/19.
+//  Copyright © 2019 Adam Dama. All rights reserved.
+//
+
+import Foundation
+import AWSAppSync
